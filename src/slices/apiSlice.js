a39823f8fe11d2d7279fa5361d5c35c3505460e6
Medo-Mohamed <146781688+Medo-Mohamed@users.noreply.github.com>
2026-05-15 21:53:14 +0300

    4

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -2,8 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
-    prepareHeaders: (headers, { getState }) => {
+    // baseUrl: "http://localhost:5000",
+    baseUrl: "https://shop-server-gupk.onrender.com",
+    prepareHeaders: (headers) => {
       headers.set("credentials", "include"); // يخبر المتصفح بالتعامل مع الكوكيز
       return headers;
     },
